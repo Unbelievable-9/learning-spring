dcbcf77067ae01a9e25747e1025e243b9e21a422
unbelievable9 <286932903@qq.com>
2017-11-17 18:09:49 +0800

    Minor Fix

--- Section1/Chapter-1/mario/src/main/java/info/unbelievable9/MarioMain.java
+++ Section1/Chapter-1/mario/src/main/java/info/unbelievable9/MarioMain.java
@@ -4,7 +4,6 @@ import info.unbelievable9.po.Mario;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * Copyright 2017 (C) Unbelievable9
--- Section1/Chapter-1/mario/src/main/java/info/unbelievable9/po/SuperMario.java
+++ Section1/Chapter-1/mario/src/main/java/info/unbelievable9/po/SuperMario.java
@@ -3,13 +3,19 @@ package info.unbelievable9.po;
 import info.unbelievable9.di.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
  * Copyright 2017 (C) Unbelievable9
  * Created on : 2017/11/16
  * Author     : Unbelievable9
  **/
-public class SuperMario implements Mario {
+public class SuperMario implements Mario, BeanNameAware, BeanFactoryAware, ApplicationContextAware,
+        BeanPostProcessor, InitializingBean, DisposableBean {
 
     private static final Logger logger = LogManager.getLogger(SuperMario.class);
 
@@ -29,5 +35,54 @@ public class SuperMario implements Mario {
         this.coinCount += stage.coinBonus();
 
         logger.trace("Mario Has: " + this.coinCount + " Coins.");
+    }
+
+
+    /**
+     * Spring Bean Life Cycle
+     */
+
+    @Override
+    public void setBeanName(String s) {
+        logger.debug("Set Bean Name.");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        logger.debug("Set Bean Factory.");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        logger.debug("Set Application Context.");
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        logger.debug("Post Process Before Initialization.");
+
+        // 没调用
+
+        return null;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logger.debug("After Properties Set.");
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+        logger.debug("Post Process After Initialization.");
+
+        // 没调用
+
+        return null;
+    }
+
+
+    @Override
+    public void destroy() throws Exception {
+        logger.debug("Destroy.");
     }
 }
