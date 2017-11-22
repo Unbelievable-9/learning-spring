3d41f6c92db42471fb9e9d46184f7972ccce34c8
unbelievable9 <286932903@qq.com>
2017-11-22 15:19:41 +0800

    Initial Java Config Demo Project.

---
+++ Section1/Chapter-2/computer-java/src/main/java/info/unbelievable9/computer/bean/FileExplorer.java
@@ -0,0 +1,19 @@
+package info.unbelievable9.computer.bean;
+
+import info.unbelievable9.computer.Software;
+
+/**
+ * Copyright 2017 (C) Unbelievable9
+ * Created on : 2017/11/21
+ * Author     : Unbelievable9
+ **/
+public class FileExplorer implements Software {
+
+    private String name = "File Explorer";
+    private String version = "1.0.0";
+
+    @Override
+    public void start() {
+        System.out.println(name + "(" + version + ") is Loading.");
+    }
+}
