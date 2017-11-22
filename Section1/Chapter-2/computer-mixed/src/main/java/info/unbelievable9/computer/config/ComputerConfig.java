3a8a26f107e9fe93afc55056549ad599bc17ebcd
unbelievable9 <286932903@qq.com>
2017-11-22 18:36:21 +0800

    Initial Mixed Configuration Project.

---
+++ Section1/Chapter-2/computer-mixed/src/main/java/info/unbelievable9/computer/config/ComputerConfig.java
@@ -0,0 +1,14 @@
+package info.unbelievable9.computer.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Copyright 2017 (C) Unbelievable9
+ * Created on : 2017/11/22
+ * Author     : Unbelievable9
+ **/
+@Configuration
+@ComponentScan(basePackages = {"info.unbelievable9.computer.bean"})
+public class ComputerConfig {
+}
---
+++ Section1/Chapter-2/computer-mixed/src/test/java/info/unbelievable9/test/SoftwareTest.java
@@ -0,0 +1,46 @@
+package info.unbelievable9.test;
+
+import info.unbelievable9.computer.Software;
+import info.unbelievable9.computer.SystemService;
+import info.unbelievable9.computer.config.ComputerConfig;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Copyright 2017 (C) Unbelievable9
+ * Created on : 2017/11/22
+ * Author     : Unbelievable9
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ComputerConfig.class)
+public class SoftwareTest {
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+    @Autowired
+    private Software software;
+
+    @Autowired
+    private SystemService systemService;
+
+    @Test
+    public void softwareShouldNotBeNull() {
+        Assert.assertNotNull(software);
+    }
+
+    @Test
+    public void systemServiceShouldOperate() {
+        systemService.operation();
+
+        Assert.assertEquals(
+                "File Explorer(1.0.0) is Loading.\n",
+                systemOutRule.getLog());
+    }
+}
